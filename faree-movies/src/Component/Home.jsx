ecfb01a444b13153ad2eeba9ed102d8c4ac4796e
ghifaryadnann <ghifaryadnann@gmail.com>
2023-12-25 22:02:03 +0700

    update last Aos Website Movie

--- faree-movies/src/Component/Home.jsx
+++ faree-movies/src/Component/Home.jsx
@@ -1,18 +1,24 @@
 import bgfaree from "../assets/bg-faree.jpg";
+import Aos from "aos";
+import 'aos/dist/aos.css'
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    Aos.init({duration:2000})
+  },[])
   return (
     <div className=" overflow-hidden bg-cyan-900 ">
       <div className=" relative bg-cyan-900">
-        <div className="absolute flex w-full flex-col mt-[50px] text-center items-center h-3/5 justify-center transition-transform">
-          <h1 className="text-[50px]  font-black">WELCOME.</h1>
-          <p className="text-2xl font-black text-shadow-lg text-amber-500">
+        <div  className="absolute flex w-full flex-col mt-[50px] text-center items-center h-3/5 justify-center transition-transform">
+          <h1 data-aos="fade-right" className="text-[50px]  font-black">WELCOME.</h1>
+          <p  data-aos="fade-left" className="text-2xl font-black text-shadow-lg text-amber-500">
             Nonton Gratis Tanpa Karcis
             
            
           </p>
          <div>
-               <p>Dengan Sengaja Menciptakan Website ini untuk Kaum Mokondo seperti kalian</p>
+               <p data-aos="fade-up">Dengan Sengaja Menciptakan Website ini untuk Kaum Mokondo seperti kalian</p>
             </div>
         </div>
 
